6f626edb841f4c9f9116c0d381872153ff566e02
Sridhakshana <dhakshana2307@gmail.com>
2026-08-03 22:51:14 +0530

    Configure MySQL database and SQLAlchemy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from app.database import engine
 
 app = FastAPI(
     title="Product Warranty Claim Processing Platform API",
     version="1.0.0"
 )
-
+@app.on_event("startup")
+def startup():
+    try:
+        connection = engine.connect()
+        print("✅ Database connected successfully!")
+        connection.close()
+    except Exception as e:
+        print(f"❌ Database connection failed: {e}")
 @app.get("/")
 def root():
     return {
         "message": "Welcome to Product Warranty Claim Processing Platform API"
     }
+    
